c6ce945748294408e5d3e49197c29fe36595f6dc
kreditsdev <65797135+kreditsdev@users.noreply.github.com>
2020-07-21 14:57:16 +0530

    zeromq updated
    Windows build fix

--- contrib/depends/packages/zeromq.mk
+++ contrib/depends/packages/zeromq.mk
@@ -1,9 +1,8 @@
 package=zeromq
-$(package)_version=4.3.2
-$(package)_download_path=https://github.com/zeromq/libzmq/releases/download/v$($(package)_version)/
+$(package)_version=4.3.3
+$(package)_download_path=https://github.com/kreditsdev/libzmq/releases/download/v$($(package)_version)/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=ebd7b5c830d6428956b67a0454a7f8cbed1de74b3b01e5c33c5378e22740f763
-$(package)_patches=b3123a2fd1e77cbdceb5ee7a70e796063b5ee5b9.patch 87b81926aaaea70c84d5a5ea6eda982b3425ceeb.patch
+$(package)_sha256_hash=3CCF273C1E81387611788D41462341EA2671DC42C2964EF11C65379DE445A51B
 $(package)_dependencies=sodium
 
 define $(package)_set_vars
@@ -11,12 +10,6 @@ define $(package)_set_vars
   $(package)_config_opts_linux=--with-pic
   $(package)_config_opts_freebsd=--with-pic
   $(package)_cxxflags=-std=c++11
-endef
-
-define $(package)_preprocess_cmds
-  patch -p1 < $($(package)_patch_dir)/b3123a2fd1e77cbdceb5ee7a70e796063b5ee5b9.patch && \
-  patch -p1 < $($(package)_patch_dir)/87b81926aaaea70c84d5a5ea6eda982b3425ceeb.patch && \
-  ./autogen.sh
 endef
 
 define $(package)_config_cmds
